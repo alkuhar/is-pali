cdc15fc0faa3368bbbafe19820578badf961331c
alkuhar <alex.kuhare@gmail.com>
2016-05-30 23:32:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# is-pali
+
+Determines if a string or number is a palindrome.
+
+Installation
+------------
+
+```bash
+npm install is-pali
+```
+
+Usage
+-------
+
+```js
+var pali = require('is-pali')
+
+pali('Amor, Roma') // true
+pali('race car') // true
+pali('10001') // true
+pali('100100') // false
+
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+* Takes a strings or number and returns true if palindrome else false.
+*
+* @param {string} str String of characters
+* @returns {boolean} bool Returns true or false
+*
+*/
+
+function palindrome(str) {
+  return str.toString().toLowerCase().split(',')
+  .join('').split(' ').join('').split('!')
+  .join('') === str.toString().toLowerCase().split(',')
+  .join('').split(' ').join('').split('!')
+  .join('').split('').reverse().join('') ? true : false;
+}
+
+module.exports = palindrome;
